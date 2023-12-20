f026ba635e01e59d9eeb7b82139c069568842335
Bryan-ro <bryangomesrocha@gmail.com>
2023-12-19 22:47:37 -0300

    prisma config and shortUrl code

--- src/urls/Url.service.ts
+++ src/urls/Url.service.ts
@@ -19,6 +19,7 @@ export default class UrlService {
                 return {
                     originalUrl: url.originalUrl,
                     shortUrl: `${serverUrl}/${url.shortUrl}`,
+                    shortUrlCode: url.shortUrl,
                     clicksQuantity: url.clicksQuantity
                 }
             }),
